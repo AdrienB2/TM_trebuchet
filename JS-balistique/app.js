8682b43278762cf22701679d1f2ca0c4a6d1e5d8
AdrienB2 <64729512+AdrienB2@users.noreply.github.com>
2021-03-01 08:35:56 +0100

    Update app.js

--- JS-balistique/app.js
+++ JS-balistique/app.js
@@ -4,22 +4,28 @@ function lancer() {
     var vy = parseFloat(document.getElementById('vy').value);
     var ay = parseFloat(document.getElementById('a').value);
 
-    var elem = document.querySelector('.circle'); 
-    var simulation = document.querySelector('.simulation'); 
-    var t = 0;
-    var interval = setInterval(frame, 10);
-    elem.style.display = "block";
-    function frame() {
-        x = vx * t/100;
-        y = simulation.clientHeight - ((0.5 * ay * (t/100) ** 2) + (vy * (t/100)) + yi);
-        if (y > simulation.clientHeight) {
-            clearInterval(interval);
-            elem.style.display = "none";
-        }
-        else {
-            elem.style.left = x + 'px';
-            elem.style.top = y + 'px';
-            t += 10;
-        }
-    }
+    // var elem = document.querySelector('.circle'); 
+    // var simulation = document.querySelector('.simulation'); 
+    // var t = 0;
+    // var interval = setInterval(frame, 10);
+    // elem.style.display = "block";
+    // function frame() {
+    //     x = vx * t/100;
+    //     y = simulation.clientHeight - ((0.5 * ay * (t/100) ** 2) + (vy * (t/100)) + yi);
+    //     if (y > simulation.clientHeight) {
+    //         clearInterval(interval);
+    //         elem.style.display = "none";
+    //     }
+    //     else {
+    //         elem.style.left = x + 'px';
+    //         elem.style.top = y + 'px';
+    //         t += 10;
+    //     }
+    // }
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
 }
