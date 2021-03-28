0dce9506c840f8b5722cea85e120558ec7471ae4
AdrienB2 <64729512+AdrienB2@users.noreply.github.com>
2021-03-28 21:39:52 +0200

    Update app.js

--- JS-balistique/app.js
+++ JS-balistique/app.js
@@ -2,15 +2,43 @@ var simulation = document.getElementById('simulation');
 simulation.width = window.innerWidth;
 simulation.height = 500;
 
+var c = simulation.getContext("2d");
+
+function drawTrebuchet(a, stopAngle) {
+    c.lineWidth = 2.5;
+    c.strokeStyle = "#fff";
+    c.clearRect(0, 0, simulation.width, simulation.height);
+    c.beginPath();
+    c.moveTo(200, simulation.height);
+    c.lineTo(200, simulation.height - 150); 
+    c.stroke();
+    c.beginPath();
+    c.moveTo(200 - Math.cos(a * Math.PI /180) * 75, simulation.height - 100 - Math.sin(a * Math.PI /180) * 75);
+    c.lineTo(200 - Math.cos(a * Math.PI /180) * 75, simulation.height - 150 - Math.sin(a * Math.PI /180) * 75);
+    c.lineTo(200 + Math.cos(a * Math.PI /180) * 75, simulation.height - 150 + Math.sin(a * Math.PI /180) * 75);
+    c.stroke();
+}
+drawTrebuchet(0, 0);
+function rotate() {
+    angle = 0;
+    stopAngle = 45;
+    var interval = setInterval(function () {
+        drawTrebuchet(angle, stopAngle);
+        angle += 1;
+    }, 10)
+    
+}
 function lancer() {
     var yi = parseFloat(document.getElementById('yi').value);
-    var vx = parseFloat(document.getElementById('vx').value);
-    var vy = parseFloat(document.getElementById('vy').value);
-    var ay = parseFloat(document.getElementById('a').value);
+    var v = parseFloat(document.getElementById('v').value);
+    var ay = parseFloat(document.getElementById('g').value);
+    var a = parseFloat(document.getElementById('a').value);
+
+    var vy = v * Math.cos(a * Math.PI / 180);
+    var vx = v * Math.cos(a * Math.PI / 180)
 
     var startButton = document.getElementById("startBtn");
     startButton.disabled = true;
-    var c = simulation.getContext("2d");
     var t = 0;
 
     var pos = [];
@@ -25,19 +53,17 @@ function lancer() {
         else {
             c.clearRect(0, 0, simulation.width, simulation.height);
             pos.push([x, y]);
-            console.log(pos);
             c.moveTo(pos[0].x, pos[0].y);
             c.beginPath();
             for (let i = 1; i < pos.length; i++) {
                 const element = pos[i];
-                console.log(element);
-                c.lineTo(Math.floor(element[0]), Math.floor(element[1]));
+                c.lineTo(element[0], element[1]);
             }
             c.lineWidth = 2.5;
             c.strokeStyle = "#1CC1BC";
             c.stroke();
             c.beginPath();
-            c.arc(Math.floor(x), Math.floor(y), 10, 0, 2 * Math.PI, false); //forme du rond
+            c.arc(x, y, 10, 0, 2 * Math.PI, false); //forme du rond
             c.fillStyle = '#fff'; //couleur, remplissage et épaisseur 
             c.fill();
             t += 10;
